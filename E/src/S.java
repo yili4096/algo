c6d46ba3ddfe7ff2efc7f86078b9d9241b8df106
yili4096 <yili4096@gmail.com>
2022-05-14 22:47:28 -0400

    55

--- E/src/S.java
+++ E/src/S.java
@@ -8,7 +8,7 @@ public class S {
 
 		String s = "中国";
 		for (char c : s.toCharArray()) {
-			//System.out.println(c);
+			System.out.println(c);
 		}
 		
 		String str = "长 短 高 低 方 圆 黑 白 红 绿 黄 蓝 灰 齐王想， 我的皮肤不痛也不痒， 怎么会有病呢？ 就摇摇头对扁鹊说：”先生，你错了， 我很健康， 什么病也没有。";
